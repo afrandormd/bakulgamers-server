176879b2d141b6cb76dfb15f086ea35f28aaf2cb
afrandormd <afrandormd@gmail.com>
2026-02-17 21:57:10 +0700

    feat: create edit data category

--- app/category/controller.js
+++ app/category/controller.js
@@ -31,5 +31,32 @@ module.exports = {
         } catch (error) {
             console.log(error)
         }
-    }
+    },
+    viewEdit: async (req, res) => {
+        try {
+            const { id } = req.params;
+            const category = await Category.findOne({ _id: id })
+
+            res.render('admin/category/edit', {
+                category
+            })
+        } catch (error) {
+            console.log(error)
+        }
+    },
+    actionEdit: async (req, res) => {
+        try {
+            const { id } = req.params
+            const { name } = req.body
+            let category = await Category.findOneAndUpdate({
+                _id: id
+            }, { name })
+
+            res.redirect('/category')
+
+        } catch (error) {
+            console.log(error)
+        }
+    },
+
 }
