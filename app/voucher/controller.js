bec76825943ba486a92416fa4e73cefb8517e176
afrandormd <afrandormd@gmail.com>
2026-02-21 02:31:12 +0700

    feat: create view data voucher & refactoring

--- app/voucher/controller.js
+++ app/voucher/controller.js
@@ -12,6 +12,8 @@ module.exports = {
             const alertStatus = req.flash("alertStatus")
             const alert = { message: alertMessage, status: alertStatus }
             const voucher = await Voucher.find()
+            .populate('category')
+            .populate('nominals')
 
             res.render('admin/voucher/view_voucher', {
                 voucher,
