a808bc4701d0338df14622164c8d5ba749af774b
afrandormd <afrandormd@gmail.com>
2026-02-23 21:44:30 +0700

    feat: create router & view create bank

---
+++ app/bank/controller.js
@@ -0,0 +1,99 @@
+const Bank = require('./model')
+
+module.exports = {
+    index: async (req, res) => {
+        try {
+            const alertMessage = req.flash("alertMessage")
+            const alertStatus = req.flash("alertStatus")
+            const alert = { message: alertMessage, status: alertStatus } 
+            const bank = await Bank.find()
+
+            res.render('admin/bank/view_bank', {
+                bank,
+                alert
+            })
+        } catch (error) {
+            req.flash("alertMessage", `${error.message}`)
+            req.flash("alertStatus", "danger")
+            res.redirect("/bank")
+        }
+    },
+    viewCreate: async (req, res) => {
+        try {
+            res.render('admin/bank/create')
+        } catch (error) {
+            req.flash("alertMessage", `${error.message}`)
+            req.flash("alertStatus", "danger")
+            res.redirect("/bank")
+        }
+    },
+    // actionCreate: async (req, res) => {
+    //     try {
+    //         const { coinName, coinQuantity, price } = req.body
+    //
+    //         let bank = await Bank({ coinName, coinQuantity, price })
+    //         await bank.save();
+    //
+    //         req.flash("alertMessage", "Berhasil tambah bank")
+    //         req.flash("alertStatus", "success")
+    //
+    //         res.redirect('/bank')
+    //
+    //     } catch (error) {
+    //         req.flash("alertMessage", `${error.message}`)
+    //         req.flash("alertStatus", "danger")
+    //         res.redirect("/bank")
+    //     }
+    // },
+    // viewEdit: async (req, res) => {
+    //     try {
+    //         const { id } = req.params;
+    //         const bank = await Bank.findOne({ _id: id })
+    //
+    //         res.render('admin/bank/edit', {
+    //             bank
+    //         })
+    //     } catch (error) {
+    //         req.flash("alertMessage", `${error.message}`)
+    //         req.flash("alertStatus", "danger")
+    //         res.redirect("/bank")
+    //     }
+    // },
+    // actionEdit: async (req, res) => {
+    //     try {
+    //         const { id } = req.params
+    //         const { coinName, coinQuantity, price } = req.body
+    //         await Bank.findOneAndUpdate({
+    //             _id: id
+    //         }, { coinName, coinQuantity, price })
+    //
+    //         req.flash("alertMessage", "Berhasil ubah bank")
+    //         req.flash("alertStatus", "success")
+    //
+    //         res.redirect('/bank')
+    //
+    //     } catch (error) {
+    //         req.flash("alertMessage", `${error.message}`)
+    //         req.flash("alertStatus", "danger")
+    //         res.redirect("/bank")
+    //     }
+    // },
+    // actionDelete: async (req, res) => {
+    //     try {
+    //         const { id } = req.params
+    //         await Bank.findOneAndRemove({
+    //             _id: id
+    //         })
+    //
+    //         req.flash("alertMessage", "Berhasil hapus bank")
+    //         req.flash("alertStatus", "danger")
+    //
+    //         res.redirect('/bank')
+    //
+    //     } catch (error) {
+    //         req.flash("alertMessage", `${error.message}`)
+    //         req.flash("alertStatus", "danger")
+    //         res.redirect("/bank")
+    //     }
+    // },
+}
