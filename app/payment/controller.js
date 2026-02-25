03020abe04f307a450d7b8348ff7efae5d4d83a9
afrandormd <afrandormd@gmail.com>
2026-02-25 08:10:00 +0700

    feat: create data payment

--- app/payment/controller.js
+++ app/payment/controller.js
@@ -1,11 +1,12 @@
 const Payment = require('./model')
+const Bank = require('../bank/model')
 
 module.exports = {
     index: async (req, res) => {
         try {
             const alertMessage = req.flash("alertMessage")
             const alertStatus = req.flash("alertStatus")
-            const alert = { message: alertMessage, status: alertStatus } 
+            const alert = { message: alertMessage, status: alertStatus }
             const payment = await Payment.find()
 
             res.render('admin/payment/view_payment', {
@@ -18,33 +19,34 @@ module.exports = {
             res.redirect("/payment")
         }
     },
-    // viewCreate: async (req, res) => {
-    //     try {
-    //         res.render('admin/payment/create')
-    //     } catch (error) {
-    //         req.flash("alertMessage", `${error.message}`)
-    //         req.flash("alertStatus", "danger")
-    //         res.redirect("/payment")
-    //     }
-    // },
-    // actionCreate: async (req, res) => {
-    //     try {
-    //         const { coinName, coinQuantity, price } = req.body
-    //
-    //         let payment = await Payment({ coinName, coinQuantity, price })
-    //         await payment.save();
-    //
-    //         req.flash("alertMessage", "Berhasil tambah payment")
-    //         req.flash("alertStatus", "success")
-    //
-    //         res.redirect('/payment')
-    //
-    //     } catch (error) {
-    //         req.flash("alertMessage", `${error.message}`)
-    //         req.flash("alertStatus", "danger")
-    //         res.redirect("/payment")
-    //     }
-    // },
+    viewCreate: async (req, res) => {
+        try {
+            const banks = await Bank.find()
+            res.render('admin/payment/create', { banks })
+        } catch (error) {
+            req.flash("alertMessage", `${error.message}`)
+            req.flash("alertStatus", "danger")
+            res.redirect("/payment")
+        }
+    },
+    actionCreate: async (req, res) => {
+        try {
+            const { banks, type } = req.body
+
+            let payment = await Payment({ banks, type })
+            await payment.save();
+
+            req.flash("alertMessage", "Berhasil tambah payment")
+            req.flash("alertStatus", "success")
+
+            res.redirect('/payment')
+
+        } catch (error) {
+            req.flash("alertMessage", `${error.message}`)
+            req.flash("alertStatus", "danger")
+            res.redirect("/payment")
+        }
+    },
     // viewEdit: async (req, res) => {
     //     try {
     //         const { id } = req.params;
