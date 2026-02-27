99eee8ee5c1b2985189e09ac6b7d1026d6462f87
afrandormd <afrandormd@gmail.com>
2026-02-27 14:38:28 +0700

    feat: create action signin admin

--- app/users/controller.js
+++ app/users/controller.js
@@ -1,3 +1,6 @@
+const User = require('./model')
+const bcrypt = require('bcryptjs')
+
 module.exports = {
     viewSignin: async (req, res) => {
         try {
@@ -14,4 +17,37 @@ module.exports = {
             res.redirect("/payment")
         }
     },
+    actionSignin: async (req, res) => {
+        try {
+            const { email, password } = req.body
+            const checkUser = await User.findOne({ email: email })
+            if (checkUser) {
+                if (checkUser.status === 'Y') {
+                    const checkPasswod = await bcrypt.compare(password, checkUser.password)
+                    if (checkPasswod){
+                        res.redirect('/dashboard')
+                    } else {
+                        req.flash("alertMessage", `Kata sandi yang anda inputkan salah`)
+                        req.flash("alertStatus", "danger")
+                        res.redirect("/")
+                    }
+
+                } else {
+                    req.flash("alertMessage", `Mohon maaf status anda belum aktif`)
+                    req.flash("alertStatus", "danger")
+                    res.redirect("/")
+                }
+
+            } else {
+                req.flash("alertMessage", `Email yang anda inputkan tidak ada`)
+                req.flash("alertStatus", "danger")
+                res.redirect("/")
+            }
+
+        } catch (error) {
+            req.flash("alertMessage", `${error.message}`)
+            req.flash("alertStatus", "danger")
+            res.redirect("/")
+        }
+    }
 }
