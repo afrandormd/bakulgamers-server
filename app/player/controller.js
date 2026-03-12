5bf87da4664231c8df1d15ef0a8713c35d3386b3
afrandormd <afrandormd@gmail.com>
2026-03-12 17:49:49 +0700

    feat: create API detail page player

--- app/player/controller.js
+++ app/player/controller.js
@@ -1,5 +1,5 @@
-const Player = require ("./model")
-const Voucher = require ("../voucher/model")
+const Player = require("./model")
+const Voucher = require("../voucher/model")
 
 module.exports = {
     landingPage: async (req, res) => {
@@ -10,7 +10,24 @@ module.exports = {
 
             res.status(200).json({ data: voucher })
         } catch (error) {
-            res.status(500).json({ message: error.message || `Terjadi kesalahan pada server` })
+            res.status(500).json({ message: error.message || `Internal Server Error` })
+
+        }
+    },
+    detailPage: async (req, res) => {
+        try {
+            const { id } = req.params
+            const voucher = await Voucher.findOne({ _id: id })
+                .populate('category')
+                .populate('nominals')
+                .populate('user', '_id name phoneNumber')
+
+            if (!voucher) {
+                return res.status(404).json({ message: "Voucher game tidak ditemukan!" })
+            }
+            res.status(200).json({ data: voucher })
+        } catch (error) {
+            res.status(500).json({ message: error.message || `Internal Server Error` })
 
         }
     }
